c7ae26038ae589adc6d17b021e1c7c728b7b144e
Tiago Azevedo <tiagogadenz@icloud.com>
2024-08-21 14:18:53 -0300

    Fix: fixed the linkedList and implemented the Recursive search

--- AED II/estrutura de dados/linked_list.py
+++ AED II/estrutura de dados/linked_list.py
@@ -9,64 +9,107 @@ class LinkedList:
         self.start = None
         self.end = None
         self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
             
     def insert(self, info, index):
-        if index > self.length:
+        if index > self.length or index < 0:
             raise IndexError("Index out of range")
-        elif self.start is None:
-            self.start = Node(info)
-            self.end = self.start
-        else:
-            middle = self.end - self.start + 1 
-            if index <= middle:
-                current = self.start
-                for _ in range(index):
-                    current = current.next
-                new_node = Node(info)
-                new_node.next = current.next
-                current.next = new_node
-            else:
-                current = self.end
-                for _ in range(middle, index):
-                    current = current.next
-                new_node = Node(info)
-                new_node.next = current.next
-                current.next = new_node
-                self.end = new_node
-        self.length += 1
+        new_node = Node(info)
         
-    def remove(self, index):
-        if index >= self.length:
-            raise IndexError("Index out of range")
-        
-        current = self.start
-        if index == 0:
-            self.start = self.start.next
-        else:
+        if self.is_empty():
+            self.start = new_node
+            self.end = new_node
+        elif index == 0:  # Insert at the beginning
+            new_node.next = self.start
+            self.start.previous = new_node
+            self.start = new_node
+        elif index == self.length:  # Insert at the end
+            self.end.next = new_node
+            new_node.previous = self.end
+            self.end = new_node
+        else:  # Insert in the middle
+            current = self.start
             for _ in range(index - 1):
                 current = current.next
-            current.next = current.next.next
+            new_node.next = current.next
+            new_node.previous = current
+            if current.next:
+                current.next.previous = new_node
+            current.next = new_node
+        self.length += 1
+
+    def append(self, info):
+        self.insert(info, self.length)
+
+    def remove(self, index):
+        if index >= self.length or index < 0:
+            raise IndexError("Index out of range")
+        
+        if index == 0:  # Remove from the beginning
+            self.start = self.start.next
+            if self.start:
+                self.start.previous = None
+            else:
+                self.end = None
+        elif index == self.length - 1:  # Remove from the end
+            self.end = self.end.previous
+            if self.end:
+                self.end.next = None
+            else:
+                self.start = None
+        else:  # Remove from the middle
+            current = self.start
+            for _ in range(index):
+                current = current.next
+            current.previous.next = current.next
+            if current.next:
+                current.next.previous = current.previous
         self.length -= 1
-    
-    def position(self, info):
-        pass
-    
-    def value(self, index):
-        pass
-    
-    def destroy(self):
-        pass
+
+    def index_search_rec(self, node, info, index=0):
+        # Base case: If node is None, the element is not in the list
+        if node is None:
+            return False
+        # If the current node's info matches, return the index
+        if node.info == info:
+            return index
+        # Recursively search in the next node until some base case is reached
+        return self.index_search_rec(node.next, info, index + 1)
+
+    def search(self, info):
+        return self.index_search_rec(self.start, info)
+
+    def reverse(self):
+        current = self.start
+        self.end = self.start
+        previous = None
+        while current is not None:
+            next_node = current.next
+            current.next = previous
+            current.previous = next_node
+            previous = current
+            current = next_node
+        self.start = previous
     
     def show(self):
         string = ""
         current = self.start
         while current is not None:
-            string += str(current.info) + ", "
+            string += str(current.info) + " -> "
             current = current.next
-        return string
+        return string.rstrip(" -> ")
     
+# Use case
 lista = LinkedList()
+lista.append(0)
+lista.append(1)
+lista.append(2)
+lista.append(3)
+lista.append(4)
+lista.append(10)
+print(lista.show())  
 
-lista.insert(1, 0)
-lista.insert(2, 4)
-print(lista.show())
+print(lista.search(10))  # Output: 1 (index of element 4)
+print(lista.search(6))  # Output: False (element 6 not found)
