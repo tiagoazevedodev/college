115ff4c735120e9c9da7c62fd281e72b32215e06
Tiago Azevedo <tiagogadenz@icloud.com>
2024-08-28 14:54:56 -0300

    Auto-commit: 2024-08-28 14:53:56

---
+++ AED II/estrutura de dados/piramideAed1.py
@@ -0,0 +1,11 @@
+n = 7
+
+for i in range(n+1):
+    for j in range(1, i+1):
+        print(j, end="")
+    print()
+for i in range(n-1, 0, -1):
+    for j in range(1, i+1):
+        print(j, end="")
+    print()
+    
