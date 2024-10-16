01a7bd77c2e5df33dd2c92afd5e4bc904d973a9b
Tiago Azevedo <tiagogadenz@icloud.com>
2024-10-16 14:46:52 -0300

    Auto-commit: 2024-10-16 14:41:41

--- AED II/estrutura de dados/binaryTree.py
+++ AED II/estrutura de dados/binaryTree.py
@@ -1,5 +1,8 @@
-from linked_list import Node
-
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 class BinaryTree:
     def __init__(self):
-        pass
+        
